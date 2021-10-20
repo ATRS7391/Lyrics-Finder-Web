7a1d8fb008e7bf83adc49607855691500439c939
ATRS <82661604+ATRS7391@users.noreply.github.com>
2021-10-20 21:39:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getQuery() {
 }
 
 function getLyrics(query){
-    url = "https://LyApi.atrs7391.repl.co/API/searchLyrics/freeLyrics/" + processQuery(query);
+    url = "https://webAPIs.lostinmemoriesanddreams.repl.co/API/searchLyrics/freeLyrics/" + processQuery(query);
     fetch(url).then((response)=>{
         return response.json();
     }).then((data)=>{
